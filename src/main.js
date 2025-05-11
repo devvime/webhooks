b494905c3bb243c3eae5af4490a960039800c7b5
devvime <dihapp@outlook.com>
2025-05-11 00:38:54 -0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5000;
 const SECRET_TOKEN = process.env.SECRET_TOKEN;
 
 const webhooks = {
-  susumo: {
-    action: "git -C /home/viimee/susumo/website pull"
+  webhook1: {
+    action: "git -C /home/user/project pull"
   }
 };
 
@@ -18,8 +18,8 @@ app.post("/git-webhook/:name", (req, res) => {
   const token = req.headers["x-hub-token"];
 
   if (token !== SECRET_TOKEN) {
-    console.warn("Tentativa de acesso com token inválido.");
-    return res.status(403).send("Token inválido");
+    console.warn("Attempted access with invalid token.");
+    return res.status(403).send("Invalid token");
   }
 
   const hook = webhooks[req.params.name];
@@ -27,8 +27,8 @@ app.post("/git-webhook/:name", (req, res) => {
   if (hook) {
     exec(hook.action, (error, stdout, stderr) => {
       if (error) {
-        console.error(`Erro ao executar comando: ${error.message}`);
-        return res.status(500).send("Erro ao executar comando");
+        console.error(`Error executing command: ${error.message}`);
+        return res.status(500).send("Error executing command");
       }
       if (stderr) {
         console.error(`stderr: ${stderr}`);
@@ -37,10 +37,10 @@ app.post("/git-webhook/:name", (req, res) => {
       res.status(200).send("OK");
     });
   } else {
-    res.status(404).send("Webhook não encontrado.");
+    res.status(404).send("Webhook not found.");
   }
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
